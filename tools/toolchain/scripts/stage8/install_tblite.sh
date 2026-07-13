ac7951d0caaade82d2575f16b4b9afdfd7888d69
Thomas D. Kuehne <tkuehne@cp2k.org>
2026-07-13 15:21:16 +0200

    Pin tested save_tblite integration revision

--- tools/toolchain/scripts/stage8/install_tblite.sh
+++ tools/toolchain/scripts/stage8/install_tblite.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_NAME")/.." && pwd -P)"
 
 tblite_ver="0.7.0"
 tblite_sha256="3a7cb4602101e828caf41c38ca5e30f82de82d0d26d5db40168acdcad3462b92"
-save_tblite_rev="85cc01e0de19c7386eef6ea927429dd6aa67b11a"
+save_tblite_rev="841ba6706144275698c923a80d4d223e160d4399"
 save_tblite_repo="${SAVE_TBLITE_REPOSITORY:-https://github.com/DCM-Uni-Paderborn/save_tblite.git}"
 save_tblite_src_dir="save_tblite-${save_tblite_rev}"
 
