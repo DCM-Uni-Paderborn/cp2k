8b6b512e5353951484e9c392d306167f34bc00a0
Thomas D. Kuehne <tkuehne@cp2k.org>
2026-07-13 21:23:56 +0200

    Document g-XTB k-point derivatives

--- tools/toolchain/scripts/stage8/install_tblite.sh
+++ tools/toolchain/scripts/stage8/install_tblite.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_NAME")/.." && pwd -P)"
 
 tblite_ver="0.7.0"
 tblite_sha256="3a7cb4602101e828caf41c38ca5e30f82de82d0d26d5db40168acdcad3462b92"
-save_tblite_rev="d3cf442a14328ab6be2aeaaa462f81c6618a8014"
+save_tblite_rev="1b5d50163edb14bd4f381c67492f3f486461547f"
 save_tblite_repo="${SAVE_TBLITE_REPOSITORY:-https://github.com/DCM-Uni-Paderborn/save_tblite.git}"
 save_tblite_src_dir="save_tblite-${save_tblite_rev}"
 
