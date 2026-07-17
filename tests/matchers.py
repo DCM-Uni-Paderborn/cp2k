715f6c9fdb116e72ba38024ec1bd9d5cb1b45bcd
Thomas D. Kuehne <tkuehne@cp2k.org>
2026-07-17 04:09:11 +0200

    Project cross-mesh k-point restarts spectrally
    (cherry picked from commit dc75e6ed9276a3139fb7aa2fdc90f6fcdf9b115f)
    Signed-off-by: Thomas D. Kühne <tkuehne@cp2k.org>

--- tests/matchers.py
+++ tests/matchers.py
@@ -90,6 +90,9 @@ registry["OT_added_mos_auto_grow"] = TextPresenceMatcher(
 registry["KPOINT_RESTART_VALIDATED_TRANSFER"] = TextPresenceMatcher(
     "KPOINT_RESTART| Validated BvK mesh transfer accepted"
 )
+registry["KPOINT_RESTART_STRICT_TRANSFER"] = TextPresenceMatcher(
+    "KPOINT_RESTART| Strict same-mesh restart accepted"
+)
 registry["GXTB_native_raw"] = TextPresenceMatcher("GXTB-Raw")
 registry["GXTB_native_simple"] = TextPresenceMatcher("GXTB-Simple")
 registry["GXTB_native_fdiis"] = TextPresenceMatcher("GXTB-FDIIS")
